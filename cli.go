5abe6f6fd36a09ff05246885d96c30896d70a84b
longkey1 <longkey1@gmail.com>
2018-07-28 06:58:04 +0900

    Add list action

--- cli.go
+++ cli.go
@@ -10,8 +10,8 @@ import (
 	"text/template"
 	"time"
 
-	"github.com/blang/semver"
 	"github.com/BurntSushi/toml"
+	"github.com/blang/semver"
 	"github.com/mitchellh/go-homedir"
 	"github.com/rhysd/go-github-selfupdate/selfupdate"
 	"github.com/urfave/cli"
@@ -33,12 +33,13 @@ type Config struct {
 	BaseDirectory string `toml:"base_directory"`
 	FileName      string `toml:"file_name"`
 	OpenCommand   string `toml:"open_command"`
+	ListCommand   string `toml:"list_command"`
 	SearchCommand string `toml:"search_command"`
 }
 
 // CLI ...
 type CLI struct {
-	inStream io.Reader
+	inStream  io.Reader
 	outStream io.Writer
 	errStream io.Writer
 }
@@ -53,17 +54,17 @@ func (c *CLI) Run(args []string) int {
 	app.Usage = "text file generator for diary"
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
-			Name: "config, c",
-			Usage: "Load configration from `FILE`",
+			Name:        "config, c",
+			Usage:       "Load configration from `FILE`",
 			Destination: &configPath,
-			Value: defaultConfigPath(),
+			Value:       defaultConfigPath(),
 		},
 	}
 	app.Commands = []cli.Command{
 		{
-			Name: "open",
+			Name:    "open",
 			Aliases: []string{"o"},
-			Usage: "open file",
+			Usage:   "open file",
 			Action: func(ctx *cli.Context) error {
 				cnf, err := loadConfig(configPath)
 				if err != nil {
@@ -80,11 +81,35 @@ func (c *CLI) Run(args []string) int {
 					file.Close()
 				}
 
-				cmd, err := c.buildCommand(cnf.OpenCommand, cnf.BaseDirectory, cnf.FileName, ""); if err != nil {
+				cmd, err := c.buildCommand(cnf.OpenCommand, cnf.BaseDirectory, cnf.FileName, "")
+				if err != nil {
 					return err
 				}
 
-				err = cmd.Run(); if err != nil {
+				err = cmd.Run()
+				if err != nil {
+					return err
+				}
+
+				return nil
+			},
+		}, {
+			Name:    "list",
+			Aliases: []string{"l"},
+			Usage:   "list files",
+			Action: func(ctx *cli.Context) error {
+				cnf, err := loadConfig(configPath)
+				if err != nil {
+					return err
+				}
+
+				cmd, err := c.buildCommand(cnf.ListCommand, cnf.BaseDirectory, cnf.FileName, "")
+				if err != nil {
+					return err
+				}
+
+				err = cmd.Run()
+				if err != nil {
 					return err
 				}
 
@@ -100,11 +125,13 @@ func (c *CLI) Run(args []string) int {
 					return err
 				}
 
-				cmd, err := c.buildCommand(cnf.SearchCommand, cnf.BaseDirectory, cnf.FileName, ctx.Args().First()); if err != nil {
+				cmd, err := c.buildCommand(cnf.SearchCommand, cnf.BaseDirectory, cnf.FileName, ctx.Args().First())
+				if err != nil {
 					return err
 				}
 
-				err = cmd.Run(); if err != nil {
+				err = cmd.Run()
+				if err != nil {
 					return err
 				}
 
@@ -115,22 +142,23 @@ func (c *CLI) Run(args []string) int {
 			Aliases: []string{"su"},
 			Usage:   "self update",
 			Action: func(ctx *cli.Context) error {
-			  v := semver.MustParse(Version)
-			  latest, err := selfupdate.UpdateSelf(v, "longkey1/diary"); if err != nil {
-			  	return err
+				v := semver.MustParse(Version)
+				latest, err := selfupdate.UpdateSelf(v, "longkey1/diary")
+				if err != nil {
+					return err
 				}
 
-			  if latest.Version.Equals(v) {
-			  	// latest version is the same as current version. It means current binary is up to date.
-			  	log.Println("current binary is the latest version", Version)
-			  } else {
-			  	log.Println("successfully updated to version", latest.Version)
-			  	log.Println("release note:\n", latest.ReleaseNotes)
+				if latest.Version.Equals(v) {
+					// latest version is the same as current version. It means current binary is up to date.
+					log.Println("current binary is the latest version", Version)
+				} else {
+					log.Println("successfully updated to version", latest.Version)
+					log.Println("release note:\n", latest.ReleaseNotes)
 				}
 
 				return nil
 			},
-	  },
+		},
 	}
 	app.Writer = c.outStream
 	app.ErrWriter = c.errStream
@@ -147,8 +175,8 @@ func (c *CLI) buildCommand(tpl string, dir string, file string, pattern string) 
 	t := template.Must(template.New("").Parse(tpl))
 	cmdbuf := new(bytes.Buffer)
 	err := t.Execute(cmdbuf, map[string]interface{}{
-		"TodayFile": buildTodayFile(dir, file),
-		"Pattern": pattern,
+		"TodayFile":     buildTodayFile(dir, file),
+		"Pattern":       pattern,
 		"BaseDirectory": dir,
 	})
 	if err != nil {
