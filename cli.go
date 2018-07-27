b1525381568131bb3f7da8b50723fc337eb0ab28
longkey1 <longkey1@gmail.com>
2018-07-27 23:31:08 +0900

    Clean code

--- cli.go
+++ cli.go
@@ -119,7 +119,8 @@ func (c *CLI) Run(args []string) int {
 			  v := semver.MustParse(Version)
 			  latest, err := selfupdate.UpdateSelf(v, "longkey1/diary"); if err != nil {
 			  	return err
-			  }
+				}
+
 			  if latest.Version.Equals(v) {
 			  	// latest version is the same as current version. It means current binary is up to date.
 			  	log.Println("current binary is the latest version", Version)
@@ -127,6 +128,7 @@ func (c *CLI) Run(args []string) int {
 			  	log.Println("successfully updated to version", latest.Version)
 			  	log.Println("release note:\n", latest.ReleaseNotes)
 				}
+
 				return nil
 			},
 	  },
